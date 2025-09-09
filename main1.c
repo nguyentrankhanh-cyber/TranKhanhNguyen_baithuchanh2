cc04754b0c7e62787cbe28f8f7ad777b270de1a1
nguyentrankhanh-cyber <nguyentk910@gmail.com>
2025-09-09 20:42:44 +0700

    Add files via upload

---
+++ main1.c
@@ -0,0 +1,30 @@
+#include "stm32f10x.h"                  // Device header
+#define RCC_APB2ENR   (*(volatile unsigned int*)0x40021018)
+#define GPIOC_CRL     (*(volatile unsigned int*)0x40011000)
+#define GPIOC_ODR     (*(volatile unsigned int*)0x4001100C)
+
+void delay(void);
+void delay(void){
+
+for( int i=0 ; i <500000; i++);
+}
+
+
+int main(void){
+
+    RCC_APB2ENR |= (1 << 4);
+
+    
+    GPIOC_CRL &= ~(0xF << 24);  //clear
+    GPIOC_CRL |=  (0x1 << 24);
+
+    
+    while(1) {
+        
+        GPIOC_ODR &= ~(1 << 6);
+        delay();
+				GPIOC_ODR |=( 1 << 6 );
+				delay();
+   
+    }
+	}
